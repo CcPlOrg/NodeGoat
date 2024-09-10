b61933215d83cc90c45908a6fed68c896fedd370
Jiaxi Zhang <NJXZhang@users.noreply.github.com>
2024-09-09 21:00:41 -0400

    chore: modify fake secret

--- config/env/all.js
+++ config/env/all.js
@@ -10,6 +10,6 @@ module.exports = {
     cryptoAlgo: "aes256",
     hostName: "localhost",
     environmentalScripts: [],
-    shopify_app_password: 'shppa_00000000000000000000000000000001'
+    shopify_app_password: 'shppa_00000000000000000000000000000000'
 };
 
